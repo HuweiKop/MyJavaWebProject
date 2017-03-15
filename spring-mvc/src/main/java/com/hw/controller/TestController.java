02eb937d7ea17365e7f5ab11d4f81fa00241b61b
[whu@ideabinder.com] <whu@ideabinder.com>
2017-03-15 17:08:13 +0800

    尝试 springmvc 自定义 实现

---
+++ spring-mvc/src/main/java/com/hw/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.hw.controller;
+
+import com.hw.annotation.Controller;
+import com.hw.annotation.RequestMapping;
+
+/**
+ * Created by Wei Hu (J) on 2017/3/10.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+}
